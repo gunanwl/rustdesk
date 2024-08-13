521a74ff054fe4a2d0bc00abb02d32e160376ef0
gunanwl <86506753+gunanwl@users.noreply.github.com>
2024-08-13 15:32:27 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rd1.ntpizza.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "uHOuYpcGHCAD5ij+SOZVgFA9V29iuTwV8iShehw17R0=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["UYDTQP.ruvip.vip"];
+pub const PUBLIC_RS_PUB_KEY: &str = "LnSokTbKXPS5DMezekSSUw";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
